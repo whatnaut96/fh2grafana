f09ee1aaf03e26cd8d1bfd0f5f4fc1308a9397de
Kenneth Hill <khill0996@gmail.com>
2026-09-24 20:51:26 -0400

    Finished packet parser

--- cmd/fh2grafana/main.go
+++ cmd/fh2grafana/main.go
@@ -1,11 +1,10 @@
-// TODO: Packet comments copy from actual page: https://support.forza.net/hc/en-us/articles/51744149102611-Forza-Horizon-6-Data-Out-Documentation
-
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
+	"fmt"
 	"log/slog"
-	"math"
 	"net"
 	"os"
 )
@@ -14,217 +13,160 @@ import (
 const ExpectedPacketSize = 324
 
 type forzaHorizon6Packet struct {
-	IsRaceOn                             int32
-	TimestampMS                          uint32
-	EngineMaxRPM                         float32
-	EngineIdleRPM                        float32
-	CurrentEngineRPM                     float32
-	VelocityX                            float32
-	VelocityY                            float32
-	VelocityZ                            float32
-	AngularVelocityX                     float32
-	AngularVelocityY                     float32
-	AngularVelocityZ                     float32
-	Yaw                                  float32
-	Pitch                                float32
-	Roll                                 float32
+	// IsRaceOn is 1 when race/driving data is active and 0 in menus or stopped states.
+	IsRaceOn int32
+
+	// TimestampMS can eventually overflow back to 0.
+	TimestampMS uint32
+
+	// Engine RPM values.
+	EngineMaxRPM     float32
+	EngineIdleRPM    float32
+	CurrentEngineRPM float32
+
+	// Acceleration in the car's local space; X = right, Y = up, Z = forward.
+	AccelerationX float32
+	AccelerationY float32
+	AccelerationZ float32
+
+	// Velocity in the car's local space; X = right, Y = up, Z = forward.
+	VelocityX float32
+	VelocityY float32
+	VelocityZ float32
+
+	// Angular velocity in the car's local space, in rad/s; X = pitch, Y = yaw, Z = roll.
+	AngularVelocityX float32
+	AngularVelocityY float32
+	AngularVelocityZ float32
+
+	// Car orientation in radians.
+	Yaw   float32
+	Pitch float32
+	Roll  float32
+
+	// Suspension travel normalized: 0.0 = max stretch, 1.0 = max compression.
 	NormalizedSuspensionTravelFrontLeft  float32
 	NormalizedSuspensionTravelFrontRight float32
 	NormalizedSuspensionTravelRearLeft   float32
 	NormalizedSuspensionTravelRearRight  float32
-	TireSlipRatioFrontLeft               float32
-	TireSlipRatioFrontRight              float32
-	TireSlipratioRearLeft                float32
-	TireSlipRatioRearRight               float32
-	WheelOnRumbleStripFrontLeft          int32
-	WheelOnRumbleStripFrontRight         int32
-	WheelOnRumbleStripRearLeft           int32
-	WheelOnRumbleStripRearRight          int32
 
-	TireCombinedSlipFrontLeft        float32
-	TireCombinedSlipFrontRight       float32
-	TireCombinedSlipRearLeft         float32
-	TireCombinedSlipRearRight        float32
+	// Tire normalized slip ratio: 0 means 100% grip, |ratio| > 1.0 means loss of grip.
+	TireSlipRatioFrontLeft  float32
+	TireSlipRatioFrontRight float32
+	TireSlipRatioRearLeft   float32
+	TireSlipRatioRearRight  float32
+
+	// Wheel rotation speed in rad/s.
+	WheelRotationSpeedFrontLeft  float32
+	WheelRotationSpeedFrontRight float32
+	WheelRotationSpeedRearLeft   float32
+	WheelRotationSpeedRearRight  float32
+
+	// WheelOnRumbleStrip is 1 when the wheel is on a rumble strip and 0 otherwise.
+	WheelOnRumbleStripFrontLeft  int32
+	WheelOnRumbleStripFrontRight int32
+	WheelOnRumbleStripRearLeft   int32
+	WheelOnRumbleStripRearRight  int32
+
+	// WheelInPuddle is 1 when the wheel is in a puddle and 0 otherwise.
+	WheelInPuddleFrontLeft  int32
+	WheelInPuddleFrontRight int32
+	WheelInPuddleRearLeft   int32
+	WheelInPuddleRearRight  int32
+
+	// Non-dimensional surface rumble values passed to controller force feedback.
+	SurfaceRumbleFrontLeft  float32
+	SurfaceRumbleFrontRight float32
+	SurfaceRumbleRearLeft   float32
+	SurfaceRumbleRearRight  float32
+
+	// Tire normalized slip angle: 0 means 100% grip, |angle| > 1.0 means loss of grip.
+	TireSlipAngleFrontLeft  float32
+	TireSlipAngleFrontRight float32
+	TireSlipAngleRearLeft   float32
+	TireSlipAngleRearRight  float32
+
+	// Tire normalized combined slip: 0 means 100% grip, |slip| > 1.0 means loss of grip.
+	TireCombinedSlipFrontLeft  float32
+	TireCombinedSlipFrontRight float32
+	TireCombinedSlipRearLeft   float32
+	TireCombinedSlipRearRight  float32
+
+	// Actual suspension travel in meters.
 	SuspensionTravelMetersFrontLeft  float32
 	SuspensionTravelMetersFrontRight float32
 	SuspensionTravelMetersRearLeft   float32
 	SuspensionTravelMetersRearRight  float32
-	CarOrdinal                       int32
-	CarClass                         int32
-	CarPerformanceIndex              int32
-	DriveTrainType                   int32
-	NumCylinders                     int32
-	CarGroup                         uint32
-	SmashableVelDiff                 float32
-	SmashableMass                    float32
-	PositionX                        float32
-	PositionY                        float32
-	PositionZ                        float32
-	Speed                            float32
-	Power                            float32
-	Torque                           float32
-	TireTempFrontLeft                float32
-	TireTempFrontRight               float32
-	TireTempRearLeft                 float32
-	TireTempRearRight                float32
-	Boost                            float32
 
-	// This is unused in FH games. More important if FM 2023 becomes a target
-	Fuel float32
+	// Car identifiers and classification.
+	CarOrdinal          int32
+	CarClass            int32
+	CarPerformanceIndex int32
+	DriveTrainType      int32
+	NumCylinders        int32
+	CarGroup            uint32
 
-	DistanceTraveled            float32
-	BestLap                     float32
-	LastLap                     float32
-	CurrentLap                  float32
-	CurrentRaceTime             float32
-	LapNumber                   uint16
-	Acceleration                uint8
-	Brake                       uint8
-	Clutch                      uint8
-	Handbrake                   uint8
-	Gear                        uint8
-	Steer                       uint8
+	// Smashable object collision data.
+	SmashableVelDiff float32
+	SmashableMass    float32
+
+	// Position in world space, in meters.
+	PositionX float32
+	PositionY float32
+	PositionZ float32
+
+	// Speed is meters per second, power is watts, torque is newton-meters.
+	Speed  float32
+	Power  float32
+	Torque float32
+
+	// Tire temperature.
+	TireTempFrontLeft  float32
+	TireTempFrontRight float32
+	TireTempRearLeft   float32
+	TireTempRearRight  float32
+
+	// Boost is PSI above atmospheric. Fuel is 0.0 empty to 1.0 full.
+	Boost float32
+	Fuel  float32
+
+	// Distance is meters. Lap and race times are seconds and 0.0 when not applicable.
+	DistanceTraveled float32
+	BestLap          float32
+	LastLap          float32
+	CurrentLap       float32
+	CurrentRaceTime  float32
+
+	// Race and player input state.
+	LapNumber    uint16
+	RacePosition uint8
+	Acceleration uint8
+	Brake        uint8
+	Clutch       uint8
+	Handbrake    uint8
+	Gear         uint8
+
+	// Signed inputs range from -127 to 127.
+	Steer                       int8
 	NormalizedDrivingLine       int8
 	NormalizedAIBrakeDifference int8
+
+	// The documented fields account for 323 bytes; FH6 packets include this trailing byte.
+	Padding uint8
 }
 
 func parsePacket(rawBuffer []byte) (forzaHorizon6Packet, error) {
-	isRaceOn := int32(binary.LittleEndian.Uint32(rawBuffer[0:4]))
-	timestamp := binary.LittleEndian.Uint32(rawBuffer[4:8])
-	engineMaxRPM := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[8:12]),
-	)
-
-	engineIdleRPM := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[12:16]),
-	)
-
-	currentEngineRPM := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[16:20]),
-	)
-
-	velocityX := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[20:24]),
-	)
-
-	velocityY := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[24:28]),
-	)
-
-	angularVelocityX := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[28:32]),
-	)
-
-	angularVelocityY := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[32:36]),
-	)
-
-	angularVelocityZ := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[36:40]),
-	)
-
-	yaw := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[40:44]),
-	)
-
-	pitch := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[44:48]),
-	)
-
-	roll := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[48:52]),
-	)
-
-	// not typing all this out for these next names.
-	// see above for what they are and where the usage is in the packet
-	nstfl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[52:56]),
-	)
-
-	nstfr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[56:60]),
-	)
-
-	nstrl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[60:64]),
-	)
-
-	nstrr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[64:68]),
-	)
-
-	tsrfl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[68:72]),
-	)
-
-	tsrfr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[72:76]),
-	)
-
-	tsrrl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[76:80]),
-	)
-
-	tsrrr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[80:84]),
-	)
-
-	rumbleStripFL := int32(binary.LittleEndian.Uint32(rawBuffer[84:88]))
-	rumbleStripFR := int32(binary.LittleEndian.Uint32(rawBuffer[88:92]))
-	rumbleStripRL := int32(binary.LittleEndian.Uint32(rawBuffer[92:96]))
-	rumbleStripRR := int32(binary.LittleEndian.Uint32(rawBuffer[96:100]))
-
-	// NOTE: The gap occurs here because I don't need the rumble fields.
-	// Going back to short names after our glimpse of light above
-	tcsfl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[116:120]),
-	)
-
-	tcsfr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[120:124]),
-	)
-
-	tcsrl := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[124:128]),
-	)
-
-	tcsrr := math.Float32frombits(
-		binary.LittleEndian.Uint32(rawBuffer[128:132]),
-	)
-
-	returnPacket := forzaHorizon6Packet{
-		IsRaceOn:                             isRaceOn,
-		TimestampMS:                          timestamp,
-		EngineMaxRPM:                         engineMaxRPM,
-		EngineIdleRPM:                        engineIdleRPM,
-		CurrentEngineRPM:                     currentEngineRPM,
-		VelocityX:                            velocityX,
-		VelocityY:                            velocityY,
-		AngularVelocityX:                     angularVelocityX,
-		AngularVelocityY:                     angularVelocityY,
-		AngularVelocityZ:                     angularVelocityZ,
-		Yaw:                                  yaw,
-		Pitch:                                pitch,
-		Roll:                                 roll,
-		NormalizedSuspensionTravelFrontLeft:  nstfl,
-		NormalizedSuspensionTravelFrontRight: nstfr,
-		NormalizedSuspensionTravelRearLeft:   nstrl,
-		NormalizedSuspensionTravelRearRight:  nstrr,
-		TireSlipRatioFrontLeft:               tsrfl,
-		TireSlipRatioFrontRight:              tsrfr,
-		TireSlipratioRearLeft:                tsrrl,
-		TireSlipRatioRearRight:               tsrrr,
-		WheelOnRumbleStripFrontLeft:          rumbleStripFL,
-		WheelOnRumbleStripFrontRight:         rumbleStripFR,
-		WheelOnRumbleStripRearLeft:           rumbleStripRL,
-		WheelOnRumbleStripRearRight:          rumbleStripRR,
-		TireCombinedSlipFrontLeft:            tcsfl,
-		TireCombinedSlipFrontRight:           tcsfr,
-		TireCombinedSlipRearLeft:             tcsrl,
-		TireCombinedSlipRearRight:            tcsrr,
+	var packet forzaHorizon6Packet
+	if len(rawBuffer) != ExpectedPacketSize {
+		return packet, fmt.Errorf("invalid packet size: got %d bytes, expected %d", len(rawBuffer), ExpectedPacketSize)
 	}
 
-	return returnPacket, nil
+	err := binary.Read(bytes.NewReader(rawBuffer), binary.LittleEndian, &packet)
+	if err != nil {
+		return packet, fmt.Errorf("parse FH6 packet: %w", err)
+	}
+
+	return packet, nil
 }
 
 func main() {
@@ -255,7 +197,11 @@ func main() {
 			slog.Error("invalid packet size", "received", n, "expected", ExpectedPacketSize)
 			continue
 		}
-		packet, err := parsePacket(buffer)
+		packet, err := parsePacket(buffer[:n])
+		if err != nil {
+			slog.Error("could not parse packet", "error", err)
+			continue
+		}
 		slog.Info("testing IRO", "val", packet.IsRaceOn)
 	}
 }
